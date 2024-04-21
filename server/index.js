d56dd2c8c2b24ffc8cb681db509a8942b1d50371
Abdelrhman Elabbasy <elabbasyabdelrhman@gmail.com>
2024-04-21 10:02:45 +0400

    connecting to mognoDB

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//connect to mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//test toutes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
